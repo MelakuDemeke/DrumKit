111a3a20f5b4aff6000680073f52acbfbfe57604
MelakuDemeke <melaku.demeke789@gmail.com>
2022-07-04 13:56:26 +0300

    key and button event listner added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        this.style.color = "white";
+        var buttonInnerHtml = this.innerHTML;
+        keyChecker(buttonInnerHtml);
     });
 }
 
 
-// var audio = new Audio('sounds/crash.mp3');
-// audio.play();
+document.addEventListener("keypress", function(event){
+    
+});
+
